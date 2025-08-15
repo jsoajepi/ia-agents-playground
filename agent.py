fedaaebd03d8040f8064b541804623349c7c3954
jsoajepi <jsoaje@piconsulting.com.ar>
2025-08-15 11:13:04 -0300

    Add wiki search tool

--- agent.py
+++ agent.py
@@ -25,6 +25,22 @@ weather_tool = Tool(
     description="Obtiene el clima actual de una ciudad"
 )
 
+# Herramienta 2: Wikipedia
+def search_wikipedia(query: str) -> str:
+    try:
+        summary = wikipedia.summary(query, sentences=2)
+        return summary
+    except wikipedia.exceptions.DisambiguationError as e:
+        return f"Demasiadas opciones: {e.options[:3]}"
+    except wikipedia.exceptions.PageError:
+        return "No encontré información en Wikipedia."
+
+wikipedia_tool = Tool(
+    name="Wikipedia",
+    func=search_wikipedia,
+    description="Busca información en Wikipedia"
+)
+
 # 2. LLM Azure OpenAI (GPT-4o)
 azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT", "<TU_ENDPOINT_AZURE_OPENAI>")
 api_key = os.getenv("AZURE_OPENAI_API_KEY", "<TU_API_KEY>")
@@ -38,16 +54,21 @@ llm = AzureChatOpenAI(
     temperature=0
 )
 
-
-# 3. Inicializar agente con la herramienta
+# 3. Inicializar agente con las herramienta
 agent = initialize_agent(
-    tools=[weather_tool],
+    tools=[weather_tool, wikipedia_tool],
     llm=llm,
     agent="zero-shot-react-description",
     verbose=True
 )
 
-
 # 4. Pregunta al agente
-response = agent.run("quien sos vos?")
-print(response)
+
+# 4. Bucle de preguntas al agente
+while True:
+    user_query = input("Que queres saber? Escribe tu pregunta para el agente (o 'salir' para terminar): ")
+    if user_query.lower() in ["salir", "exit"]:
+        print("¡Hasta luego!")
+        break
+    response = agent.run(user_query)
+    print(response)
